2d8c2ff924c0551bb8e8d001f5a2507d9dc7a4c8
Timo <Timo@TIMO-PC>
2024-09-20 08:46:22 +0200

    Projektdateien hinzufügen.

---
+++ LastEpochShared/Services/EventService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastEpochShared.Services
+{
+    public static class EventService
+    {
+        public static event EventHandler<string> CallPageSwitch;
+
+        public static void ChangePage(string nameOfPage)
+        {
+            if (string.IsNullOrEmpty(nameOfPage))
+            {
+                return;
+            }
+
+            CallPageSwitch?.Invoke(null, nameOfPage);
+        }
+    }
+}
